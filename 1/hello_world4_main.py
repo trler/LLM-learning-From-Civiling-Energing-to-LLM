c94aff5bce5ba94d6454c9d145946dee4d2d351c
trler <952450968@qq.com>
2025-08-12 19:18:33 +0800

    更新函数,while,input

---
+++ 1/hello_world4_main.py
@@ -0,0 +1,16 @@
+#  moudle import
+
+# import
+# import
+# import
+# import
+# import
+# import
+# import
+# import
+import hello_world4_pizzy 
+
+hello_world4_pizzy.make_pizza() 
+
+from hello_world4_pizzy import make_pizza as pz
+pz()
